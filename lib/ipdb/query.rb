cdc18d5222b62b65a5de9caf22cadae66cc7afb1
Dustin Willis Webber <dustin.webber@gmail.com>
2009-11-26 01:52:51 -0600

    remove json code

--- lib/ipdb/query.rb
+++ lib/ipdb/query.rb
@@ -15,19 +15,11 @@ module Ipdb
     SCRIPT = 'http://ipinfodb.com/ip_query2.php'
 
     def initialize(addr, options={})
-      @output = (options[:output] || :xml).to_sym
       @timeout = options[:timeout] || 10
 
       ip = [addr].flatten
       @url = "#{SCRIPT}?ip=#{URI.escape(ip.join(','))}&output=#{@output}"
-      case @output
-        when :xml
-          @xml = Nokogiri::XML.parse(open(@url))
-        when :json
-          buffer = open(@url).read
-          blah = JSON.parse(buffer)
-          return blah
-      end
+      @xml = Nokogiri::XML.parse(open(@url))
     end
 
     def each(&block)
@@ -44,17 +36,11 @@ module Ipdb
       @url
     end
     
-    def to_xml
-      @xml
-    end
-    
-    def to_json
-      @xml
-    end
-    
     def to_s
-      @xml.to_s
+      @xml
     end
+
+    alias to_xml to_s
 
   end
 end
